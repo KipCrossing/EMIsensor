16ccde1a4e24569c6ae8f8bd3fdb1e396ccee9fc
kip <kip.crossing@gmail.com>
2016-11-29 18:09:01 +1100

    Fumction made - ready for matplotlib

--- Transmitter.py
+++ Transmitter.py
@@ -23,43 +23,49 @@ def small_flux_dencity(dl_point, dl, focus_point):
     return dB
 
 
-r = input("Radius: ")
-n = input("Number of slices: ")
+#r = input("Radius: ")
+#n = input("Number of slices: ")
 
-r = float(r)
-n = int(n)
+#r = float(r)
+#n = int(n)
+
+r = 1
+n = 100
 
 dl = 2*r*np.tan(pi/n)
 
 print("dl: "+ str(dl))
 
-x = 1
+def Flux_dencity(rx,ry,rz):
 
-d = [0, 0, -1]
-j = [0, 0, x]           # focus point
+    d = [0, 0, -1]		# controles current direction 
+    j = [rx, ry, rz]           # focus point
+    
+    sum = [0,0,0]
+    for i in range(n):
+        px = r*np.cos(i*(2*pi/n))
+        py = r*np.sin(i*(2*pi/n))
+    
+        v = [px, py, 0]
+        v_hat = v/np.linalg.norm(v)
+        dl_hat = np.cross(v_hat, d)
+    
+    
+        dl_v = np.dot(dl,dl_hat)
+        #print("Radius vector " + str(v)+" dl: " + str(dl_v))
+    
+        b = small_flux_dencity(v,dl_v,j)
+        #print(b)
+        sum = np.add(sum,b)
 
-sum = [0,0,0]
-for i in range(n):
-    px = r*np.cos(i*(2*pi/n))
-    py = r*np.sin(i*(2*pi/n))
+    #print("Sum: " + str(sum))
+    return sum
 
-    v = [px, py, 0]
-    v_hat = v/np.linalg.norm(v)
-    dl_hat = np.cross(v_hat, d)
-
-
-    dl_v = np.dot(dl,dl_hat)
-    #print("Radius vector " + str(v)+" dl: " + str(dl_v))
-
-    b = small_flux_dencity(v,dl_v,j)
-    #print(b)
-    sum = np.add(sum,b)
-
-print("Sum: " + str(sum))
+print(Flux_dencity(2,0,1))
 
 #print("dl: " + str(dl))
 #print("actual circumference: " + str(pi*r*2) + "   Approximate circumference: " + str(n*dl))
 
 
-B = ((permeability*I*turns*(r**2)) /(2.0*((r**2 + x**2)**0.5)**3))
-print(B)
+#B = ((permeability*I*turns*(r**2)) /(2.0*((r**2 + 0**2)**0.5)**3))		# where 0 is x 
+#print(B)

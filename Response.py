f005d598121be30a506535cabb185a9c4f3e6344
kip <kip.crossing@gmail.com>
2017-01-18 12:32:36 +1100

    Set up inputs of Flux_dencity for complete run

--- Response.py
+++ Response.py
@@ -3,23 +3,15 @@ from Euler import rotation_matrix
 import time
 
 
-Tx = [0,0,1]
-Rx = [0,0,1]
 
+n = 100            # number of slices
 
 EC = 25
 permeability = np.pi * 4 * 10 ** (-7)
 frequency = 10000       # Hz
-S = [1, 0, 0]
 turns = 1
 
-
-RTx = 0.025
-n = 100            # number of slices
-
-r = [0.5, 0, 0.5]
-
-#print((np.linalg.norm(S)**2)*EC*permeability*frequency*2*np.pi/4)
+#+print((np.linalg.norm(S)**2)*EC*permeability*frequency*2*np.pi/4)
 
 
 def small_flux(dl_point, dl, focus_point):
@@ -34,9 +26,9 @@ def small_flux(dl_point, dl, focus_point):
 
 
 
-def Flux_dencity(Axis, Tx_position, RTx, r):
-    (Axis1, Axis2) = Axis
-    (Tx_position1, Tx_position2) = Tx_position
+def Flux_dencity(Tx1, Tx2, RTx, r):
+    (Axis1, Tx_position1) = Tx1
+
 
     dl = 2 * RTx * np.tan(np.pi / n)
 
@@ -62,8 +54,9 @@ def Flux_dencity(Axis, Tx_position, RTx, r):
 
         sum = np.add(sum, b)
 
-    if Axis2 != None:
 
+    if Tx2 != None:
+        (Axis2, Tx_position2) = Tx2
         Axis_hat2 = Axis2 / np.linalg.norm(Axis2)
 
 
@@ -83,10 +76,16 @@ def Flux_dencity(Axis, Tx_position, RTx, r):
 
     return sum
 
-Rx_position = [1,0,0]
-m = 20
+Tx1 = ([0,0,1],[0.05,0,0])
+Tx2 = None
+Rx = ([0,0,1],[1.05,0,0])
+
+RTx = 0.025
+
+(Rx_axis,Rx_position) = Rx
+m = 10
 Re = 1.0/(2*float(m))
-Hp = Flux_dencity((Tx, [0,0,-1]),([0,0,0],[2,0,0]),0.05,Rx_position)
+Hp = Flux_dencity(Tx1,Tx2,0.05,Rx_position)
 bottom = np.dot(Rx,Hp)
 
 
@@ -96,13 +95,15 @@ f.write("x,y,result\n")
 for k in range(-2 * m, 0):
     for i in range(-1 * m, 3 * m + 1):
         r = [float(i)/m,0.0,float(k)/m]
-        Hr = Flux_dencity((Tx, None),([0,0,0],None),0.05,r)
-        Hs = Flux_dencity((Hr, None),(r,None),Re,Rx_position)     # Check for magnitude of Hr in Flux_dencity()
+        try:
+            Hr = Flux_dencity(Tx1,Tx2,0.05,r)
+            Hs = Flux_dencity((Hr, r),None,Re,Rx_position)     # Check for magnitude of Hr in Flux_dencity()
 
-        result = (np.linalg.norm(Hr)*(np.dot(Rx,Hs)/ np.dot(Rx,Hp)))*0.0000000001           # Change factor
-        #print(str(i / float(m)) + "," + str(k / float(m)) + "," + str(result))
-        f.write(str(i / float(m)) + "," + str(k / float(m)) + "," + str(result) + "\n")
+            result = (np.linalg.norm(Hr)*(np.dot(Rx_axis,Hs)/ np.dot(Rx_axis,Hp)))*10000           # Change factor
+            #print(str(i / float(m)) + "," + str(k / float(m)) + "," + str(result))
+            f.write(str(i / float(m)) + "," + str(k / float(m)) + "," + str(result) + "\n")
+        except RuntimeWarning:
+            print("Error")
+            pass
     print(k)
 f.close()
-
-

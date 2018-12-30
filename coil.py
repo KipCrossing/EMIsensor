12a63a2596d785fc8c54ba066b1f950e53cea51c
KipCrossing <kip.crossing@gmail.com>
2018-12-30 19:10:58 +1100

    Calculating the potential estimated flux factor  F

--- coil.py
+++ coil.py
@@ -7,10 +7,32 @@ from Euler import rotation_matrix
 class Coil(object):
     # Put some of these into init
     coil_I = 1
-    _coil_sides = 128
-    coil_radius = 0.005
+
+
     coil_position = [0,0,0]
     coil_axis = [0,0,1]
+
+    coil_radius_in = 0.005
+    coil_radius_out = 0.015
+    coil_radius_ave = (coil_radius_in + coil_radius_out)/2.0
+    coil_length = 0.065
+    coil_depth = coil_radius_out - coil_radius_in
+    wire_thickness = 0.0005
+    coil_turns = (coil_length/wire_thickness) \
+    *(coil_depth/wire_thickness)
+
+    coil_inductance = ((0.8/25.4)*(coil_radius_ave**2)*(coil_turns**2)) \
+    /(6*coil_radius_ave + 9*coil_length + 10*coil_depth)
+
+    coil_resistance = 12.0
+
+    coil_impedance = np.sqrt(coil_resistance**2 + coil_inductance**2)
+
+    coil_current = 1/coil_impedance
+    F = coil_current*coil_turns
+
+    _winding_sides = 128
+    winding_radius = coil_radius_in
 
     def __init__(self):
         pass
@@ -43,18 +65,18 @@ class Coil(object):
 
     def Flux_Dencity(self,flux_location):
 
-        small_length = 2 * self.coil_radius * np.tan(np.pi / self._coil_sides)
+        small_length = 2 * self.winding_radius * np.tan(np.pi / self._winding_sides)
 
         #Create an class that inherits numpy and includes a methof that creates the unit vector for any vector in 3D
         unit_coil_axis = self.coil_axis / np.linalg.norm(self.coil_axis)
 
         c = np.cross(self.coil_axis, [1, 0, 0])
 
-        start_radius_vector = (c / np.linalg.norm(c)) * self.coil_radius
+        start_radius_vector = (c / np.linalg.norm(c)) * self.winding_radius
 
         total_flux = [0, 0, 0]
-        for i in range(self._coil_sides):
-            new_radius_vector = np.dot(rotation_matrix(self.coil_axis, i * (2 * np.pi / self._coil_sides)), start_radius_vector)
+        for i in range(self._winding_sides):
+            new_radius_vector = np.dot(rotation_matrix(self.coil_axis, i * (2 * np.pi / self._winding_sides)), start_radius_vector)
 
             small_length_location = np.add(self.coil_position, new_radius_vector)
 
@@ -91,11 +113,8 @@ class Mesh(object):
                     self.vector_mesh[location] = self._zero_vector
 
 
-T_Mesh = Mesh(0.001,-1,3,-1,1,-2,0)
-T_Mesh.Generate_Mesh()
-print(T_Mesh.vector_mesh)
-
 
 T_Coil = Coil()
-r = T_Coil.Flux_Dencity([1,1,1])
+print(T_Coil.coil_turns)
+r = T_Coil.F
 print(r)
